7760d1d02d4c5597f3d9a83114321a0625380999
Zach Hempstead <zhempstead@uchicago.edu>
2023-07-27 02:33:11 +0000

    refactor ER script

--- llm_crowd/entity_resolution/query.py
+++ llm_crowd/entity_resolution/query.py
@@ -1,19 +1,31 @@
 import argparse
-from pathlib import Path
-
-import pandas as pd
 
 from llm_crowd.lib.utils import ROOTDIR, DATADIR
 from llm_crowd.lib.experiment_config import experiment_config
-from llm_crowd.lib.experiment import write_responses, combine_responses
+from llm_crowd.lib.experiment import write_responses, combine_responses, train_df, test_df
 from llm_crowd.entity_resolution.prompt_template import ERPromptTemplate
 
 NUM_SHOTS = 2
 
-ERDIR = DATADIR / 'entity_resolution'
+def main(experiment):
+    exp_conf = experiment_config('entity_resolution', experiment)
 
-def load_testsets(exp_conf):
-    return {d: pd.read_csv(ERDIR / 'test' / f"{d}.csv") for d in exp_conf['datasets']}
+    datasets = load_testsets(exp_conf)
+    datasets = {d: test_df('entity_resolution', d) for d in exp_conf['datasets']} 
+
+    prompt_templates = init_templates(exp_conf)
+
+    shots_datasets = load_shots_datasets(exp_conf)
+    examples_func = examples_funcs(shots_datasets)[exp_conf['shots']]
+
+    write_responses(
+        'entity_resolution',
+        experiment,
+        datasets,
+        prompt_templates,
+        examples_func,
+        exp_conf['reps'])
+    combine_responses('entity_resolution', experiment)
 
 def load_shots_datasets(exp_conf, size='small'):
     if exp_conf['shots'] == 'none':
@@ -27,7 +39,7 @@ def load_shots_datasets(exp_conf, size='small'):
     return {d: load_shots_dataset(d) for d in shots_datasets}
 
 def load_shots_dataset(dataset, size='small'):
-    df = pd.read_csv(ERDIR / 'train' / f'{dataset}-{size}.csv')
+    df = train_df('entity_resolution', dataset, size)
 
     shot_yes = df[df['label']].sample(n=len(df)*shots, replace=True, random_state=100).reset_index(drop=True)
     shot_no = df[~df['label']].sample(n=len(df)*shots, replace=True, random_state=100).reset_index(drop=True)
@@ -79,18 +91,4 @@ if __name__ == '__main__':
     parser.add_argument("experiment")
     args = parser.parse_args()
 
-    exp_conf = experiment_config('entity_resolution', args.experiment)
-
-    exp_dir = Path(ROOTDIR / 'out' / 'entity_resolution' / args.experiment)
-    raw_dir = exp_dir / 'raw'
-    raw_dir.mkdir(parents=True, exist_ok=True)
-
-    datasets = load_testsets(exp_conf)
-
-    prompt_templates = init_templates(exp_conf)
-
-    shots_datasets = load_shots_datasets(exp_conf)
-    examples_func = examples_funcs(shots_datasets)[exp_conf['shots']]
-
-    write_responses(raw_dir, datasets, prompt_templates, examples_func, exp_conf['reps'])
-    combine_responses(raw_dir, exp_dir / 'responses.csv')
+    main(args.experiment)
